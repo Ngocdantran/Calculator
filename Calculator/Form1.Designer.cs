d0076788f77f3bb1219e953f3e540c8eff7eb851
Ngocdantran <tranngocdan@tlu.edu.vn>
2025-02-15 10:44:14 +0700

    Tao project

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsoa = new System.Windows.Forms.TextBox();
+            this.txtsob = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số b";
+            // 
+            // txtsoa
+            // 
+            this.txtsoa.Location = new System.Drawing.Point(143, 58);
+            this.txtsoa.Name = "txtsoa";
+            this.txtsoa.Size = new System.Drawing.Size(100, 20);
+            this.txtsoa.TabIndex = 1;
+            // 
+            // txtsob
+            // 
+            this.txtsob.Location = new System.Drawing.Point(143, 84);
+            this.txtsob.Name = "txtsob";
+            this.txtsob.Size = new System.Drawing.Size(100, 20);
+            this.txtsob.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kết quả";
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(143, 126);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(100, 20);
+            this.txtkq.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 297);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txtsob);
+            this.Controls.Add(this.txtsoa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsoa;
+        private System.Windows.Forms.TextBox txtsob;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtkq;
+    }
+}
+
